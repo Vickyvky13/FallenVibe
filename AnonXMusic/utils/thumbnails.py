21faa0ef63b1a535ed41dc195de588303c20c4dc
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-12-01 23:28:10 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -68,7 +68,7 @@ async def get_thumb(videoid):
         image1 = changeImageSize(1280, 720, youtube)
         sex = changeImageSize(900, 380, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(40))
+        background = image2.filter(filter=ImageFilter.BoxBlur(20))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
         logo = ImageOps.expand(sex, border=5, fill="Cyan")
