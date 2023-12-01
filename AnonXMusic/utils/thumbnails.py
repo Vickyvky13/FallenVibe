14e7783e0b7b8a038f2e172e1250d25033e83bd6
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-12-01 18:31:42 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -91,14 +91,14 @@ async def get_thumb(videoid):
         )
         draw.line(
             [(55, 660), (1220, 660)],
-            fill="yellow",
+            fill="Cyan",
             width=5,
             joint="curve",
         )
         draw.ellipse(
             [(918, 648), (942, 672)],
-            outline="white",
-            fill="white",
+            outline="Cyan",
+            fill="Cyan",
             width=15,
         )
         draw.text(
